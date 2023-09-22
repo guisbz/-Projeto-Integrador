57f7448f2f3837d8cc896154f3d5b942d47a5aba
guisbz <109392433+guisbz@users.noreply.github.com>
2023-09-22 17:03:28 -0300

    Requisição funcionando corretamente

--- LootUsado/main.py
+++ LootUsado/main.py
@@ -40,20 +40,33 @@ def cadastro():
 
 @app.route('/register', methods=['POST'])
 def register():
-    usuarios = {
-        "nome": request.form.get('nome'),
-        "senha": request.form.get('senha'),
+    novo_usuario = request.form.get('nome')
+    nova_senha = request.form.get('senha')
+
+    if not novo_usuario or not nova_senha:
+        return jsonify({"error": "Nome de usuário e senha são obrigatórios!"})
+
+    # Ler os usuários existentes do arquivo JSON
+    with open('usuarios.json', 'r') as usuarios_file:
+        lista = json.load(usuarios_file)
+
+    # Verificar se o nome de usuário já existe
+    for usuario in lista:
+        if usuario['nome'] == novo_usuario:
+            return jsonify({"error": "O nome de usuário já existe. Escolha outro."})
+
+    # Adicionar o novo usuário à lista
+    novo_usuario_obj = {
+        "nome": novo_usuario,
+        "senha": nova_senha
     }
+    lista.append(novo_usuario_obj)
 
-    if all(usuarios.values()):
-        with open('usuarios.json', 'a') as file:
-            file.write(json.dumps(usuarios) + '\n')
+    # Salvar a lista atualizada de volta no arquivo JSON com a codificação correta
+    with open('usuarios.json', 'w', encoding='utf-8') as usuarios_file:
+        json.dump(lista, usuarios_file, ensure_ascii=False, indent=4)
 
-        return jsonify({"message": "Dados recebidos com sucesso!"})
-    else:
-        return jsonify({"error": "Todos os campos são obrigatórios!"})
-
-    return jsonify({"message": "Dados recebidos com sucesso!"})
+    return jsonify({"message": "Registro realizado com sucesso!"})
 
 @app.route("/")
 def home():
