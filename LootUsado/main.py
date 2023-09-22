d3c0e77a8b6e5e24123a6a37ab05970d02b63953
guisbz <109392433+guisbz@users.noreply.github.com>
2023-09-21 23:49:44 -0300

    Commit do desespero

--- LootUsado/main.py
+++ LootUsado/main.py
@@ -5,6 +5,36 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= "PALAVRA-SECRETA"
+
+
+@app.route("/cadastro", methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        novo_usuario = request.form.get('novo-usuario')
+        nova_senha = request.form.get('nova-senha')
+
+        # Valide se o nome de usuário já existe
+        with open('usuarios.json', 'r') as usuarios:
+            lista = json.load(usuarios)
+            for c in lista:
+                if novo_usuario == c['nome']:
+                    flash('O nome de usuário já existe. Escolha outro.')
+                    return redirect("/cadastro")
+
+        # Se o nome de usuário não existe, adicione o novo usuário ao arquivo JSON
+        novo_usuario_obj = {
+            "nome": novo_usuario,
+            "senha": nova_senha
+        }
+        lista.append(novo_usuario_obj)
+        with open('usuarios.json', 'w') as usuarios:
+            json.dump(lista, usuarios, indent=4)
+
+        flash('Registro realizado com sucesso. Faça o login.')
+        return redirect("/")
+    
+    return render_template("html/cadastro.html")
+
 @app.route("/")
 def home():
     return render_template("html/login.html")
@@ -13,6 +43,7 @@ def home():
 def login():
     usuario = request.form.get('nome')
     senha = request.form.get('senha')
+
     with open('usuarios.json') as usuarios:
         lista = json.load(usuarios)
         cont = 0
@@ -23,7 +54,10 @@ def login():
             if cont >= len(lista):
                 flash('usuario invalido')
                 return redirect("/")
+    print(usuario)
+    print(senha)
 
+    
 
 
 if __name__ in '__main__':
